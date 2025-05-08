626789fa133dce5c423f6f906c43761334aba619
Hiroshi Murayama <opiopan@gmail.com>
2025-05-09 00:23:25 +0900

    preparation for migrating the build system from GNU Make to CMake

--- firmware/main/component.mk
+++ firmware/main/component.mk
@@ -4,7 +4,8 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 CFLAGS		+= -DMG_ENABLE_HTTP_STREAMING_MULTIPART
 CXXFLAGS	+= -DMG_ENABLE_HTTP_STREAMING_MULTIPART -Wunused-function
-#CXXFLAGS	+= -std=c++11
+
+COMPONENT_DEPENDS		:= elflet-version
 
 COMPONENT_SRCDIRS                = . foundations
 COMPONENT_ADD_INCLUDEDIRS        = . foundations
